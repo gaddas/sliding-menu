cdff1936a7569853cdd5fd37775582298d9b5272
gaddas <gaddas@abv.bg>
2013-08-02 16:08:43 +0300

    example cleaned to run without Sherlock

--- example/src/com/jeremyfeinstein/slidingmenu/example/ExampleListActivity.java
+++ example/src/com/jeremyfeinstein/slidingmenu/example/ExampleListActivity.java
@@ -3,39 +3,31 @@ package com.jeremyfeinstein.slidingmenu.example;
 import java.net.URLEncoder;
 
 import android.app.AlertDialog;
-import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.preference.Preference;
+import android.preference.PreferenceActivity;
 import android.preference.PreferenceScreen;
 import android.text.Html;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ImageView;
-import android.widget.ListView;
-import android.widget.TextView;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Toast;
 
-import com.actionbarsherlock.app.SherlockPreferenceActivity;
-import com.actionbarsherlock.view.Menu;
-import com.actionbarsherlock.view.MenuItem;
-import com.crittercism.app.Crittercism;
 import com.jeremyfeinstein.slidingmenu.example.anim.CustomScaleAnimation;
 import com.jeremyfeinstein.slidingmenu.example.anim.CustomSlideAnimation;
 import com.jeremyfeinstein.slidingmenu.example.anim.CustomZoomAnimation;
 import com.jeremyfeinstein.slidingmenu.example.fragments.FragmentChangeActivity;
 import com.jeremyfeinstein.slidingmenu.example.fragments.ResponsiveUIActivity;
 
-public class ExampleListActivity extends SherlockPreferenceActivity {
+public class ExampleListActivity extends PreferenceActivity {
 
-	@Override
+	@SuppressWarnings("deprecation")
+        @Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setTitle(R.string.app_name);
-		
-//		Crittercism.init(getApplicationContext(), "508ab27601ed857a20000003");
+
 		this.addPreferencesFromResource(R.xml.main);
 	}
 
@@ -71,7 +63,8 @@ public class ExampleListActivity extends SherlockPreferenceActivity {
 		return true;
 	}
 
-	@Override
+	@SuppressWarnings("deprecation")
+        @Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
 		case R.id.github:
@@ -106,7 +99,7 @@ public class ExampleListActivity extends SherlockPreferenceActivity {
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		getSupportMenuInflater().inflate(R.menu.example_list, menu);
+		getMenuInflater().inflate(R.menu.example_list, menu);
 		return true;
 	}
 	
--- example/src/com/jeremyfeinstein/slidingmenu/example/fragments/BirdActivity.java
+++ example/src/com/jeremyfeinstein/slidingmenu/example/fragments/BirdActivity.java
@@ -7,17 +7,16 @@ import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.Window;
 import android.widget.ImageView;
 import android.widget.ImageView.ScaleType;
 
-import com.actionbarsherlock.app.SherlockActivity;
-import com.actionbarsherlock.view.MenuItem;
 import com.jeremyfeinstein.slidingmenu.example.R;
 
-public class BirdActivity extends SherlockActivity {
+public class BirdActivity extends Activity {
 
 	private Handler mHandler;
 	
@@ -38,13 +37,14 @@ public class BirdActivity extends SherlockActivity {
 		String[] birds = getResources().getStringArray(R.array.birds);
 		TypedArray imgs = getResources().obtainTypedArray(R.array.birds_img);
 		int resId = imgs.getResourceId(pos, -1);
+		imgs.recycle();
 		
 		setTitle(birds[pos]);
 		getWindow().requestFeature(Window.FEATURE_ACTION_BAR_OVERLAY);
 		ColorDrawable color = new ColorDrawable(Color.BLACK);
 		color.setAlpha(128);
-		getSupportActionBar().setBackgroundDrawable(color);
-		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+		getActionBar().setBackgroundDrawable(color);
+		getActionBar().setDisplayHomeAsUpEnabled(true);
 		mHandler = new Handler();
 		
 		ImageView imageView = new ImageView(this);
@@ -52,7 +52,7 @@ public class BirdActivity extends SherlockActivity {
 		imageView.setImageResource(resId);
 		imageView.setOnClickListener(new OnClickListener() {
 			public void onClick(View v) {
-				getSupportActionBar().show();
+				getActionBar().show();
 				hideActionBarDelayed(mHandler);
 			}
 		});
@@ -63,7 +63,7 @@ public class BirdActivity extends SherlockActivity {
 	@Override
 	public void onResume() {
 		super.onResume();
-		getSupportActionBar().show();
+		getActionBar().show();
 		hideActionBarDelayed(mHandler);
 	}
 	
@@ -80,7 +80,7 @@ public class BirdActivity extends SherlockActivity {
 	private void hideActionBarDelayed(Handler handler) {
 		handler.postDelayed(new Runnable() {
 			public void run() {
-				getSupportActionBar().hide();
+				getActionBar().hide();
 			}
 		}, 2000);
 	}
